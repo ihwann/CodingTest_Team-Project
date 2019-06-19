9b2d76cf6b26490aaf10de013f38c0f49039c765
daeunii94 <39181911+daeunii94@users.noreply.github.com>
2019-06-19 15:35:23 +0900

    modify reviewListService.java

--- src/main/java/com/stevelee/java/service/ReviewListService.java
+++ src/main/java/com/stevelee/java/service/ReviewListService.java
@@ -11,24 +11,22 @@ import com.stevelee.java.dao.ReviewDao;
 import com.stevelee.java.dto.ReviewDto;
 
 @Service
-public class ReviewListService implements ReviewDao{
+public class ReviewListService {
 
 	@Autowired
 	private SqlSession reviewSqlSession;
-	
-	@Autowired
 	private ReviewDao reviewDao;
 
-	@Override
-	public ArrayList<ReviewDto> select_all_review() throws SQLException, Exception {
+	
+	public ArrayList<ReviewDto> select_all_review() throws SQLException{
 		
 		reviewDao = reviewSqlSession.getMapper(ReviewDao.class);
 		
 		return reviewDao.select_all_review();
 	}
 
-	@Override
-	public ArrayList<String> select_all_review_comp() throws SQLException, Exception {
+	
+	public ArrayList<String> select_all_review_comp() throws SQLException{
 		
 		reviewDao = reviewSqlSession.getMapper(ReviewDao.class);
 		
